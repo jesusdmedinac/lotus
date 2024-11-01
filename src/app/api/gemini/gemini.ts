1591e49939f58a7ca5ec012fe360561f7c278528
Jesús Daniel Medina Cruz <jesus.daniel.medina.cruz@gmail.com>
2024-11-01 07:17:53 -0700

    Env config updated

--- src/app/api/gemini/gemini.ts
+++ src/app/api/gemini/gemini.ts
@@ -1,4 +1,6 @@
-const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.SECRET_GEMINI_API_KEY}`;
+import './envConfig.ts'
+
+const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`;
 
 export async function generateContent(
   systemInstructions: string,
