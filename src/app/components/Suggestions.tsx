c1669730086b77be64ad18530df0b0507ac7e7d2
Jesús Daniel Medina Cruz <jesus.daniel.medina.cruz@gmail.com>
2024-11-04 20:30:13 -0800

    Suggestions with Youtube video

--- src/app/components/Suggestions.tsx
+++ src/app/components/Suggestions.tsx
@@ -9,16 +9,16 @@ import { signInAnonymously } from "firebase/auth";
 import { Box, Divider, Tab, Tabs } from "@mui/material";
 import TimerIcon from '@mui/icons-material/Timer';
 import CodeIcon from '@mui/icons-material/Code';
-import Image from "next/image";
 import { Lambda2Data } from "../models/lambda2";
 import { Lambda3Data } from "../models/lambda3";
+import YouTubeComponent from "./youtube";
 
 export default function Suggestions({ url }: { url: string }) {
   const auth = useFirebaseAuth();
   const [video, setVideo] = useState<PrismaVideo | null>(null);
   const [lambda2Data, setLambda2Data] = useState<Lambda2Data | null>(null);
   const [lambda3Data, setLambda3Data] = useState<Lambda3Data | null>(null);
-  const [selectedTab, setSelectedTab] = useState(1);
+  const [selectedTab, setSelectedTab] = useState(0);
 
   useEffect(() => {
     if (!auth) return;
@@ -100,6 +100,14 @@ export default function Suggestions({ url }: { url: string }) {
   return (
     <div className="flex flex-col w-full overflow-y-scroll">
       <div className="flex flex-row w-full p-16 justify-center">
+        <div className="flex flex-row justify-center items-center w-full rounded-xl">
+          <YouTubeComponent
+            id={video.youtube.id}
+            handlePlay={() => console.log("play")}
+            handlePause={() => console.log("pause")}
+            className="w-full aspect-video border-none z-0"
+          />
+        </div>
         <div className="flex flex-col rounded-xl p-4">
           <p className="px-4 py-2 text-lg font-semibold">{video.materia}</p>
           <div className="flex flex-row gap-2">
@@ -110,67 +118,68 @@ export default function Suggestions({ url }: { url: string }) {
           <p className="px-4 py-2 text-lg font-semibold">{video.youtube.title}</p>
           <p className="px-4 py-2 text-md font-medium">{video.youtube.description.substring(0, 100) + "..."}</p>
         </div>
-        <Image 
-          src={video.youtube.thumbnail} 
-          alt="Imagen de YouTube" 
-          layout="fixed" 
-          width={96} 
-          height={54} 
-          className="rounded-xl"/>
       </div>
-      <div className="flex flex-col w-full p-16 bg-slate-900">
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={selectedTab} onChange={onTabSelected} aria-label="basic tabs example">
-          <Tab label="Contenido" {...a11yProps(0)} />
-          <Tab label="Recomendaciones" {...a11yProps(1)} />
-        </Tabs>
-      </Box>
-      <CustomTabPanel value={selectedTab} index={0}>
-        <div className="flex flex-col w-full p-16 bg-slate-900">
-          <div className="flex flex-col items-center w-full">
-            <Divider className="w-20" textAlign="center"  />
+      <div className="flex flex-col w-full py-16 bg-slate-900">
+        <Box sx={{ 
+          borderBottom: 1, 
+          borderColor: 'divider', 
+          justifyContent: 'center', 
+          display: 'flex', 
+          alignItems: 'center' 
+          }}>
+          <Tabs value={selectedTab} onChange={onTabSelected} aria-label="basic tabs example">
+            <Tab label="Contenido" {...a11yProps(0)} />
+            <Tab label="Recomendaciones" {...a11yProps(1)} />
+          </Tabs>
+        </Box>
+        <CustomTabPanel value={selectedTab} index={0}>
+          <div className="flex flex-col w-full p-16 bg-slate-900">
+            <div className="flex flex-col items-center w-full">
+              <Divider className="w-20" textAlign="center"  />
+            </div>
+            <p className="px-4 py-2 text-2xl font-semibold">Este contenido te podría interesar</p>
+            <div className="px-4 py-2 flex flex-row w-full">
+              <p className="px-4 py-2 text-md border-2 border-slate-700 rounded-lg">{lambda2Data.tipo}</p>
+            </div>
+            <p className="px-4 py-2 text-md font-medium">{lambda2Data.contenido}</p>
           </div>
-          <p className="px-4 py-2 text-2xl font-semibold">Este contenido te podría interesar</p>
-          <div className="px-4 py-2 flex flex-row w-full">
-            <p className="px-4 py-2 text-md border-2 border-slate-700 rounded-lg">{lambda2Data.tipo}</p>
-          </div>
-          <p className="px-4 py-2 text-md font-medium">{lambda2Data.contenido}</p>
-        </div>
-      </CustomTabPanel>
-      <CustomTabPanel value={selectedTab} index={1}>
-        <div className="flex flex-col w-full p-16 bg-slate-900">
-          <div className="flex flex-col items-center w-full">
-            <Divider className="w-20" textAlign="center"  />
-          </div>
-          <p className="px-4 pb-2 pt-8 text-2xl font-semibold">Este es tu siguiente paso 🏃</p>
-          <div className="flex flex-col w-full">
-            <div className="flex flex-row gap-2 w-full overflow-scroll">
-              {
-                todas.map((recomendacion, index) => (
-                  <div key={index} className="flex flex-col min-w-64 p-4 rounded-xl bg-slate-800 aspect-video">
-                    <p className="text-xl font-semibold truncate">{
-                      recomendacion.profesor ? recomendacion.profesor : "Profesor desconocido"
-                    }</p>
-                    <p className="text-md font-medium truncate">{recomendacion.titulo}</p>
-                    <div className="flex flex-row gap-2 py-2 w-full overflow-hidden flex-wrap">
-                      <p className="text-sm p-2 border-2 border-slate-700 rounded-lg">{recomendacion.estilo}</p>
-                      <p className="text-sm p-2 border-2 border-slate-700 rounded-lg">{recomendacion.canal}</p>
+        </CustomTabPanel>
+        <CustomTabPanel value={selectedTab} index={1}>
+          <div className="flex flex-col w-full py-16 bg-slate-900">
+            <div className="flex flex-col items-center w-full">
+              <Divider className="w-20" textAlign="center"  />
+            </div>
+            <p className="px-16 py-8 text-2xl font-semibold">Este es tu siguiente paso 🏃</p>
+            <div className="flex flex-col w-full">
+              <div className="flex flex-row gap-4 w-full overflow-scroll">
+                <div></div>
+                {
+                  todas.map((recomendacion, index) => (
+                    <div key={index} className="flex flex-col w-[480px] h-[270px] p-4 rounded-xl bg-slate-800 aspect-video">
+                      <p className="text-xl font-semibold truncate">{
+                        recomendacion.profesor ? recomendacion.profesor : "Profesor desconocido"
+                      }</p>
+                      <p className="text-md font-medium truncate">{recomendacion.titulo}</p>
+                      <div className="flex flex-row gap-2 py-2 w-full overflow-hidden flex-wrap">
+                        <p className="text-sm p-2 border-2 border-slate-700 rounded-lg">{recomendacion.estilo}</p>
+                        <p className="text-sm p-2 border-2 border-slate-700 rounded-lg">{recomendacion.canal}</p>
+                      </div>
+                      <div className="flex flex-row gap-2 py-2 w-full overflow-hidden flex-wrap items-center">
+                        <CodeIcon className="rounded-full bg-slate-500 p-1 size-8" />
+                        <p className="text-sm p-2 text-cyan-500 rounded-lg">{recomendacion.clase}</p>
+                      </div>
+                      <div className="flex flex-row gap-2 py-2 w-full overflow-hidden flex-wrap items-center">
+                        <TimerIcon className="rounded-full bg-slate-500 p-1 size-8" />
+                        <p className="text-sm p-2 text-cyan-500 rounded-lg">{recomendacion.duracion}</p>
+                      </div>
                     </div>
-                    <div className="flex flex-row gap-2 py-2 w-full overflow-hidden flex-wrap items-center">
-                      <CodeIcon className="rounded-full bg-slate-500 p-1 size-8" />
-                      <p className="text-sm p-2 text-cyan-500 rounded-lg">{recomendacion.clase}</p>
-                    </div>
-                    <div className="flex flex-row gap-2 py-2 w-full overflow-hidden flex-wrap items-center">
-                      <TimerIcon className="rounded-full bg-slate-500 p-1 size-8" />
-                      <p className="text-sm p-2 text-cyan-500 rounded-lg">{recomendacion.duracion}</p>
-                    </div>
-                  </div>
-                ))
-              }
+                  ))
+                }
+                <div></div>
+              </div>
             </div>
           </div>
-        </div>
-      </CustomTabPanel>
+        </CustomTabPanel>
       </div>
     </div>
   );
@@ -200,7 +209,7 @@ function CustomTabPanel(props: TabPanelProps) {
       aria-labelledby={`simple-tab-${index}`}
       {...other}
     >
-      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+      {value === index && <Box sx={{ p: 0 }}>{children}</Box>}
     </div>
   );
 }
