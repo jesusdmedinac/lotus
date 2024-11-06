59982d64e8b6642c8dad40ed6fc703fd03a8a9c8
Jesús Daniel Medina Cruz <jesus.daniel.medina.cruz@gmail.com>
2024-11-06 14:46:17 -0800

    Feedback collapsed

--- src/app/components/FeedbackComponent.tsx
+++ src/app/components/FeedbackComponent.tsx
@@ -1,19 +1,27 @@
-import { Button } from "@mui/material";
+import { Box, Button, Collapse } from "@mui/material";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function FeedbackComponent({ className }: { className?: string }) {
+  const [collapsed, setCollapsed] = useState(false);
   const onVotaClick = () => {
     window.open("https://calendly.com/jesusdmedinac/lotus-internal-testing", "_blank");
   }
-  return (<Button className={className} variant="contained" color="info" onClick={onVotaClick}>
-    <div className="flex flex-row items-center gap-2 text-black font-bold">
-      <Image
-        src="/lotus-black.svg"
-        alt="Logo Lotus"
-        width={24}
-        height={24}
-        />
-      Compartir feedback
-    </div>
-  </Button>);
+  return (
+    <Button className={className} variant="contained" color="info" onClick={onVotaClick}>
+      <Box sx={{ width: 'auto' }}>
+        <Collapse orientation="horizontal" in={collapsed} collapsedSize={24} onMouseOver={() => {setCollapsed(true)}} onMouseLeave={() => {setCollapsed(false)}}>
+          <div className="flex flex-row items-center gap-2 text-black font-bold">
+            <Image
+              src="/lotus-black.svg"
+              alt="Logo Lotus"
+              width={24}
+              height={24}
+              />
+            <p className="overflow-hidden whitespace-nowrap">Compartir feedback</p>
+          </div>
+        </Collapse>
+      </Box>
+    </Button>
+  );
 }
